148e7efb3945a3306c1e88c91475240656c9fc01
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2017-06-05 14:54:33 +0530

    Update PosNeg.java

--- PosNeg.java
+++ PosNeg.java
@@ -1,15 +1,18 @@
 import java.util.Scanner;
 class PosNeg
 {
-  Scanner s=new Scanner(System.in);
-  int a=s.nextInt();
-  if(a!=0)
-  {
-    if(a>0)
-    System.out.print("postive");
-    else
-    System.out.print("negative");
-  }
-  else
-    System.out.print("zero");
+	static public void main(String[] args)
+  	{	
+  		Scanner s=new Scanner(System.in);
+  		int a=s.nextInt();
+  		if(a!=0)
+  		{
+    		if(a>0)
+    			System.out.print("postive");
+    		else
+    			System.out.print("negative");
+  		}
+  		else
+    		System.out.print("zero");
+  	}		
 }
