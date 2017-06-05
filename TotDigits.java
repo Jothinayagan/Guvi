551b70763973e7edde8a0b104b78ee019fb2e4ba
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2017-06-05 15:51:26 +0530

    Update TotDigits.java

--- TotDigits.java
+++ TotDigits.java
@@ -3,7 +3,6 @@ class TotDigits
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		// your code goes 
 		Scanner s=new Scanner(System.in);
 		int i=s.nextInt();
 		String ss=String.valueOf(i);
