5fdaf58d4bf1e81c8eebcbd3b8bea770890512f7
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2017-06-05 15:49:44 +0530

    Create TotDigits.java

---
+++ TotDigits.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class TotDigits
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes 
+		Scanner s=new Scanner(System.in);
+		int i=s.nextInt();
+		String ss=String.valueOf(i);
+		int k=ss.length();
+		System.out.println(k);
+	}
+}
