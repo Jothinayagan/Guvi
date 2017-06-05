c709306f653a1581da137aa576d008a655593749
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2017-06-05 14:48:13 +0530

    Update PosNeg.java

--- PosNeg.java
+++ PosNeg.java
@@ -1,1 +1,15 @@
-# Guvi
+import java.util.Scanner;
+class PosNeg
+{
+  Scanner s=new Scanner(System.in);
+  int a=s.nextInt();
+  if(a!=0)
+  {
+    if(a>0)
+    System.out.print("postive");
+    else
+    System.out.print("negative");
+  }
+  else
+    System.out.print("zero");
+}
