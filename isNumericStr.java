ec9655cb45d03051eb02a9e32c5dc587f23806e5
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2017-06-11 23:25:23 +0530

    Create isNumericStr.java

--- isNumericStr.java
+++ isNumericStr.java
@@ -3,17 +3,17 @@ class isNumericStr
 {
 	public static void main(String[] args) 
 	{
-		Scanner s = new Scanner(System.in);
-		String str = s.nextLine();
-		char[] c = str.toCharArray();
-		boolean b1 = false;
-		for (int i = 0; i < str.length(); i++) 
+		Scanner s=new Scanner(System.in);
+		String str=s.nextLine();
+		char[] c=str.toCharArray();
+		boolean bool = false;
+		for(int i=0;i<str.length();i++) 
 		{
-			if (!(Character.isAlphabetic(c[i]))) 
+			if(!(Character.isAlphabetic(c[i]))) 
 			{
-				b1 = true;
+				bool=true;
 			}
 		}
-		System.out.println(b1);
+		System.out.print(bool);
 	}
 }
