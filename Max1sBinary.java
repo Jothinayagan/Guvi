ce20fcd5c0066650b60e6759f0386aefc1a0cd67
Jothinayagan <v.i.jothinayagan.307@gmail.com>
2017-06-12 10:36:38 +0530

    Create Max1sBinary.java

---
+++ Max1sBinary.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Max1sBinary
+{
+    public static void main(String[] args) 
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        String c = Integer.toBinaryString(n);
+        System.out.println(c);
+        String[] a=c.split("0");
+        Arrays.sort(a);
+        int n1=a.length;
+        System.out.print(a[n1-1]);
+	}
+}
