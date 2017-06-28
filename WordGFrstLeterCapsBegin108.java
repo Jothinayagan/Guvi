21bcf3fee68073ba53f0b79a765d824354ea7c7b
Jothinayagan Iyyappan <Jothinayagan@users.noreply.github.com>
2017-06-28 10:46:27 +0530

    Create WordGFrstLeterCapsBegin108.java

---
+++ WordGFrstLeterCapsBegin108.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+class WordGFrstLeterCapsBegin108 
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		String[] a=s.nextLine().split(" ");
+		for(int i=0;i<a.length;i++){
+		    String a1[]=a[i].split("");
+		    a1[0]=a1[0].toUpperCase();
+		    for(String c1:a1)
+		    System.out.print(c1);
+		    System.out.print(" ");	}
+}
+}
