f76f8165805ea39e455b3964527f73920c133cdc
Jothinayagan Iyyappan <Jothinayagan@users.noreply.github.com>
2017-06-28 12:03:40 +0530

    Create MinDiffBwTeamHunt46.java

---
+++ MinDiffBwTeamHunt46.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class MinDiffBwTeamHunt46 
+{
+	public static void main (String[] args) 
+	{
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		int j=n/2;
+		int h=n-j;
+		System.out.println(h+" "+j);
+		System.out.println(h-j);
+    }
+}
