a326b1cada4b676fa8edba8235adbc2f21bb609b
Jothinayagan Iyyappan <Jothinayagan@users.noreply.github.com>
2017-06-29 14:31:37 +0530

    Update RemoveArrElementHunt65.java

--- RemoveArrElementHunt65.java
+++ RemoveArrElementHunt65.java
@@ -3,7 +3,13 @@ import java.util.*;
 class RemoveArrElementHunt65 {
 	public static void main (String[] args) {
 		Scanner sr=new Scanner(System.in);
-		int[] a={10,10,12,13,14};int c=sr.nextInt();
+		int n=sr.nextInt();
+		int[] a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+		    a[i]=sr.nextInt();
+		}
+		int c=sr.nextInt();
 		for(int i=0;i<a.length;i++)
 		{
 		    if(c!=a[i])
