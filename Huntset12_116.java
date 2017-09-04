0c230d1637a84fd7d22d374ad938097e412c0fc3
Jothinayagan Iyyappan <Jothinayagan@users.noreply.github.com>
2017-09-04 07:28:24 +0530

    Create Huntset12_116.java

---
+++ Huntset12_116.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Huntset12_116 {
+	public static void main (String[] args) {
+		Scanner sr = new Scanner(System.in);
+		int m = sr.nextInt(); String str = String.valueOf(m);
+		String[] st = str.split(""); int count = 0, co = 0;
+		if(m>=0 && m<=999999){
+		for(int i = 0; i<st.length; i++)
+		{
+		    int x = Integer.valueOf(st[i]);
+		    co = (int)Math.pow(x,i); 
+		    count += co;
+		}}
+		if(count > 0){ System.out.println(count); }
+		else{ System.out.println("-1"); }
+	}
+}
