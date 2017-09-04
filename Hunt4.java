99ae02db37c0315769f665e535f2ca48db050a9d
Jothinayagan Iyyappan <Jothinayagan@users.noreply.github.com>
2017-09-04 17:28:13 +0530

    Create Hunt4.java

---
+++ Hunt4.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Hunt4 {
+	public static void main (String[] args) {
+		Scanner sr = new Scanner(System.in);
+		int n = sr.nextInt();
+		List<Integer> a = new ArrayList<Integer>();
+		for(int i = 0; i<n; i++)
+		{
+		    a.add(sr.nextInt());
+		}
+		//System.out.println(a);
+		for(int j = 0; j<n; j++)
+		{
+		    int x = Collections.frequency(a,a.get(j));
+		    if( x == 1){ System.out.print(a.get(j)+" "); }
+		}
+	}
+}
